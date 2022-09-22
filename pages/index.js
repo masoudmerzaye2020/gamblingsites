5d1304a28476f2415a1278e7f733b480979bc1e0
Masoud Merzaye <dreamteam.freelancer@gmail.com>
2022-09-22 19:57:57 +0700

    index

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,163 @@
+import Image from 'next/image'
+import Link from 'next/link'
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <div className='w-12/12 '>
+      <div className='w-full bg-[#15365a] text-center p-2'>
+        <h1 className='text-2xl font-semibold text-gray-200 py-1'>The Best Online Gambling Sites for 2022</h1>
+        <p className='text-lg text-gray-300 py-1'>Rankings and Reviews of 200+ Online Casinos, Sports Betting Sites, and Other Gambling Websites</p>
+      </div>
+      <div className='container mx-auto p-2 lg:w-8/12 lg:py-10'>
+        <p className=' '>
+       <span className='text-lg font-semibold '> GamblingSites.com is the internet’s most comprehensive guide to the world’s best online gambling sites.</span> This website is a genuine goldmine for gamblers, with information and expert advice on just about every gambling-related topic under the sun.
+
+Our main objective is to ensure you enjoy safe online betting and gaming at reputable and trustworthy gambling sites. You’ll find full details on everything else we have to offer throughout this page, but let’s start with our latest rankings of the best gambling sites.
+        </p>
+        <div className='bg-[#1f2d37]'>
+          <h1 className='text-3xl font-bold p-2 text-gray-200'>Best Gambling Sites Ranked by GamblinSites.com</h1>
+          <div className='bg-gray-200 mx-2  p-6'>
+            {/* <div className='border-2 border-[#0fdfd2] '>
+              
+
+            </div> */}
+
+
+
+
+
+            <div className='flex w-[100%] md:flex flex-wrap border-2 border-[#0fdfd2] mb-8'>
+              <div className='w-[50%] md:w-[20%] md:order-1 '>
+                <div className='py-4 mx-auto text-center border-b-[1px] border-gray-400'>
+                  <Image src="/casino/tony-bet-logo-casino-gad-table-316x160-1-1.jpg" width="192" height="80" alt='logo' /></div>
+                </div>
+                <div className='py-4 w-[50%] md:w-[30%] md:order-7 font-semibold border-b-[1px] border-gray-400 md:border-t-[1px]'>
+                  <p className='text-xl text-center font-semibold mx-4'>welcome bonus</p>
+                  <p className='text-xl text-center font-semibold mx-4'>100% up to $1,600 120 free Spins</p>
+                </div>
+
+
+              <div className=' mx-auto py-4 text-center w-[50%] md:w-[20%] md:order-4 border-b-[1px] border-gray-400'>
+                <Image src="/casino/best-choice.png" width="90" height="90" alt='logo' />
+              </div>
+
+              <div className='w-[50%] md:w-[20%] md:order-8 py-4 border-b-[1px] border-gray-400'>
+                  <div className=' uppercase font-semibold px-4 text-center text-lg'>
+                    <button className='bg-[#fccd1b] px-6 py-2 rounded-lg border-b-2 border-[#d6ae17]'> Visit Site</button>
+                  </div>
+                  <div className='text-center py-4'>
+                    <Link href="/"><a className='underline'>READ REVIEW</a></Link>
+                  </div>
+              </div>
+
+
+              <div className='px-4 pt-4 w-[100%] md:w-[35%] md:order-2'>
+                <ul>
+                  <li>
+                    <Image src='/casino/li-check.png' width="14px" height="14px" alt="image" className=' text-center flex' /> Choose from over 900 casino games
+                  </li>
+                  <li>
+                  <Image src='/casino/li-check.png' width="14px" height="14px" alt="image" className=' text-center flex' /> Play live ealer games from Pragmatic Play and Evolution Gaming
+                  </li>
+                  <li>
+                  <Image src='/casino/li-check.png' width="14px" height="14px" alt="image" className='my-auto text-center flex' /> Two deposit bonuses fro new customers
+                  </li>
+                </ul>
+              </div>
+              <div className='px-4 pb-4 w-[100%] md:w-[25%] md:order-3 border-b-[1px] border-gray-400'>
+                <ul>
+                  <li>
+                  <Image src='/casino/li-x.png' width="14px" height="14px" alt="image" className='my-auto text-center flex' /> 40x playthrough on deposit bonuses
+                  </li>
+                  <li>
+                  <Image src='/casino/li-x.png' width="14px" height="14px" alt="image" className='my-auto text-center flex' /> Not available in the US
+                  </li>
+                </ul>
+              </div>
+              <div className='px-4 py-6 w-[50%] md:w-[30%] md:order-5 md:border-t-[1px] border-gray-400'>
+                <div>
+                  <h2 className='text-lg font-semibold pb-6'>Deposit Method & Payout</h2>
+                </div>
+                <div className='flex mx-auto text-center justify-center'>
+                <Image src="/casino/visa.png" width="50px" height="30px" alt='bank'/>
+                <Image src="/casino/mastercard.png" width="50px" height="30px" alt='bank'/>
+                <Image src="/casino/bitcoin.png" width="50px" height="30px" alt='bank'/>
+                </div>
+            </div>
+            <div className='px-4 py-6 w-[50%] md:w-[20%] md:order-6 border-t-[1px] border-gray-400' > 
+              <p>Bitcoin (24 hours) Cerdit Cards (upto 7 days) E-wallets(12 hours)</p>
+            </div>           
+            </div>
+
+            {/*  */}
+            <div className='flex w-[100%] md:flex flex-wrap border-2 border-blue-500'>
+              <div className='w-[50%] md:w-[20%] md:order-1'>
+                <div className='py-4 mx-auto text-center border-b-[1px] border-gray-400'>
+                  <Image src="/casino/tony-bet-logo-casino-gad-table-316x160-1-1.jpg" width="192" height="80" alt='logo' /></div>
+                </div>
+                <div className='py-4 w-[50%] md:w-[30%] md:order-7 font-semibold border-b-[1px] border-gray-400 md:border-t-[1px]'>
+                  <p className='text-xl text-center font-semibold mx-4'>welcome bonus</p>
+                  <p className='text-xl text-center font-semibold mx-4'>100% up to $1,600 120 free Spins</p>
+                </div>
+
+
+              <div className=' mx-auto py-4 text-center w-[50%] md:w-[20%] md:order-4 border-b-[1px] border-gray-400'>
+                <Image src="/casino/best-choice.png" width="90" height="90" alt='logo' />
+              </div>
+
+              <div className='w-[50%] md:w-[20%] md:order-8 py-4 border-b-[1px] border-gray-400'>
+                  <div className=' uppercase font-semibold px-4 text-center text-lg'>
+                    <button className='bg-[#fccd1b] px-6 py-2 rounded-lg border-b-2 border-[#d6ae17]'> Visit Site</button>
+                  </div>
+                  <div className='text-center py-4'>
+                    <Link href="/"><a className='underline'>READ REVIEW</a></Link>
+                  </div>
+              </div>
+
+
+              <div className='px-4 pt-4 w-[100%] md:w-[35%] md:order-2 py-4'>
+                <ul>
+                  <li>
+                    <Image src='/casino/li-check.png' width="14px" height="14px" alt="image" className=' text-center flex' /> Choose from over 900 casino games
+                  </li>
+                  <li>
+                  <Image src='/casino/li-check.png' width="14px" height="14px" alt="image" className=' text-center flex' /> Play live ealer games from Pragmatic Play and Evolution Gaming
+                  </li>
+                  <li>
+                  <Image src='/casino/li-check.png' width="14px" height="14px" alt="image" className='my-auto text-center flex' /> Two deposit bonuses fro new customers
+                  </li>
+                </ul>
+              </div>
+              <div className='px-4 pb-4 w-[100%] md:w-[25%] md:order-3 border-b-[1px] md:border-b-[opx] border-gray-400'>
+                <ul>
+                  <li>
+                  <Image src='/casino/li-x.png' width="14px" height="14px" alt="image" className='my-auto text-center flex' /> 40x playthrough on deposit bonuses
+                  </li>
+                  <li>
+                  <Image src='/casino/li-x.png' width="14px" height="14px" alt="image" className='my-auto text-center flex' /> Not available in the US
+                  </li>
+                </ul>
+              </div>
+              <div className='px-4 py-6 w-[50%] md:w-[30%] md:order-5 '>
+                <div>
+                  <h2 className='text-lg font-semibold pb-6'>Deposit Method & Payout</h2>
+                </div>
+                <div className='flex mx-auto text-center justify-center'>
+                <Image src="/casino/visa.png" width="50px" height="30px" alt='bank'/>
+                <Image src="/casino/mastercard.png" width="50px" height="30px" alt='bank'/>
+                <Image src="/casino/bitcoin.png" width="50px" height="30px" alt='bank'/>
+                </div>
+            </div>
+            <div className='px-4 py-6 w-[50%] md:w-[20%] md:order-6' > 
+              <p>Bitcoin (24 hours) Cerdit Cards (upto 7 days) E-wallets(12 hours)</p>
+            </div>           
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>         
+      
+    </>    
   )
 }
