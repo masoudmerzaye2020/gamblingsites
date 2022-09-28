fca0383ed2526964c2857561b5c167fbc47531d6
Masoud Merzaye <masoudmerzaye@gmail.com>
2022-09-28 08:01:29 +0700

    real money

--- pages/real-money/index.js
+++ pages/real-money/index.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import Section from '../../components/section'
+import Container from '../../components/container'
+import Title from '../../components/title'
 import Header from '../../components/header'
+import Button from '../../components/button'
 
 function Index() {
   return (
     <>
         <Header>Sports Betting Guide: How to Bet On Sports For Fun & Profit</Header>
+        <Section>
+          <Container>
+            <p>It’s no surprise that a lot of people like to gamble for real money, online and in person. Playing poker, visiting casinos to enjoy the games there, or betting on sports are all fun and could bring you some rewards, too. Naturally, though, there are also a lot of risks involved.</p>
+            <Title title={'What Is Real Money Gambling?'}/>
+            <p>Put simply, real money gambling involves placing a wager on an uncertain outcome with the chance of winning or losing money depending on what outcome transpires.</p>
+            <div className='w-full md:w-8/12 mx-auto py-4'>
+              <img src='/image/01_Real-Money-Gambling-Page-Graphics_no1-1.png' width='100%' height='auto' alt='kjk'/>
+            </div>
+            <Title title={'The Most Popular Ways to Gamble Online for Real Money'}/>
+            <p>As you probably know, there are many different forms of gambling. Examples include making a bet with your friends, playing in a casino, and betting on sports with a bookmaker. As long as a real money wager is being placed on an uncertain outcome, it’s gambling.</p>
+            <div className='w-full md:w-8/12 mx-auto py-4'>
+              <img src='/image/01_Real-Money-Gambling-Page-Graphics_no2-1.png' width='100%' height='auto' alt='kjk'/>
+            </div>
+          </Container>
+        </Section>
     </>
   )
 }
