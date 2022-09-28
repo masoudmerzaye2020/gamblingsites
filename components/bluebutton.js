c155aafb71449191f2d98dd211a254e38ba2e99c
Masoud Merzaye <dreamteam.freelancer@gmail.com>
2022-09-28 13:21:03 +0700

    image issue

--- components/bluebutton.js
+++ components/bluebutton.js
@@ -3,8 +3,8 @@ import Image from 'next/image'
 
 export default function Bluebutton({children}) {
   return (
-    <div className='bg-[#06c] rounded-full flex  p-1 text-white hover:text-[#06c] hover:bg-white border-2 border-[#06c] w-5/12 mx-auto my-2'>
-        <div className='w-10/12 justify-center my-auto text-lg pl-2'>
+    <div className='bg-[#06c] rounded-full flex  p-1 text-white hover:text-[#06c] hover:bg-white border-2 border-[#06c] w-9/12 md:w-5/12 mx-auto my-2'>
+        <div className='w-10/12 justify-center my-auto text-sm md:text-lg pl-2'>
         {children}
         </div>
         <div className='w-2/12 flex my-auto justify-center  '>
--- pages/football-betting/index.js
+++ pages/football-betting/index.js
@@ -28,22 +28,23 @@ function index() {
                 <p>American football has become popular all over the world. It’s still most popular within the US, though, where it’s the country’s number one spectator sport and also attracts the most betting interest.</p>
                 <Title title={'Ranking the Best Real Money Football Betting Sites'}/>
                 <p>When it comes to ranking real money football betting sites, or any type of gambling site for that matter, we have to bear in mind that people have different preferences and requirements. The “best” site for one person may not necessarily be the best for someone else.</p>
-                <div className='my-4'>
-                    <Image src='/image/Safe-Trusted-Football-Betting-Sites.jpg' width='45' height='45' alt='football'/>
+                <div className='my-4 flex flex-wrap m-auto max-w-[1900px] h-[150px] md:h-[200px] lg:h-[300px]'>
+                    <Image className='flex w-full' src='/image/Safe-Trusted-Football-Betting-Sites.jpg' width='1800' height='100%' alt='football'/>
                 </div>
+                
                 <p>The benefits of using legal football betting sites that are subject to regulation by reputable authorities should be obvious. The last thing you want to worry about when betting on football online is whether a site is going to disappear with your money or not pay you your winnings.</p>
 
 
                 <Title title={'Why Use Online Football Betting Sites?'}/>
                 <p>There are brick-and-mortar sports betting parlors in some parts of the United States, and they are as common as convenience stores in many parts of Europe. If you have access to a physical sportsbook, why would you want to bet football online?</p>
-                <div className='my-4'>
-                    <Image src='/image/Why-Bet-On-Football-Online.jpg' width='100%' height='100%' alt='football'/>
+                <div className='my-4 flex flex-wrap m-auto max-w-[1900px] h-[150px] md:h-[200px] lg:h-[300px]'>
+                    <Image className='flex w-full' src='/image/Why-Bet-On-Football-Online.jpg' width='1800' height='100%' alt='football'/>
                 </div>
 
                 <Title title={'NFL and College Football Betting'}/>
                 <p>The best football betting sites emphasize both the National Football League and NCAA college football. Regardless of the specific flavor you prefer, the highly trusted sites that we recommend will exceed your expectations.</p>
 
-                <div className='flex flex-wrap'>
+                <div className='flex flex-wrap w-full'>
                 <Bluebutton>NFT Bettin Sites</Bluebutton>
                 <Bluebutton>College Football Betting Sites</Bluebutton>
                 <Bluebutton>College Football Betting Sites</Bluebutton>
@@ -73,6 +74,41 @@ function index() {
                     </div>
                 </div>
                 
+
+                <Title title={'Things to Remember'}/>
+                <p>
+                We’ve hit you hard with a lot of information all at one time, so in closing, we’ll provide a brief summary. First and foremost, you can place your absolute trust in all the top-ranked football betting sites for 2022. They are firmly established and totally legit in the eyes of industry insiders.
+                </p>
+                <div className='flex flex-wrap w-full '>
+                    <div className='w-8/12 md:w-6/12 lg:w-4/12 xl:w-3/12   mx-auto p-4'>
+                        <div className='w-[250px] mx-auto rounded-lg border-[2px] border-[#1f2d37] hover:border-green-400'>
+                            <Image className='flex' src='/image/Week-5-NCAAF-odds-1-250-140.jpg' width='400px' height='200px' alt=''/> 
+                            <h1 className='px-2 py-4'>College Footbvall Picks and Odds for Week 5(2022)</h1>
+                            <p className='px-2 pb-2 text-sm'>There's nothing quite like the roller coaster of college football. you always think a game will go..</p>
+                        </div>
+                    </div>
+                    <div className='w-8/12 md:w-6/12 lg:w-4/12 xl:w-3/12  mx-auto p-4'>
+                        <div className='w-[250px] mx-auto rounded-lg border-[2px] border-[#1f2d37] hover:border-green-400'>
+                            <Image src='/image/Week-5-NCAAF-odds-1-250-140.jpg' width='400px' height='200px' alt=''/> 
+                            <h1 className='px-2 py-4'>College Footbvall Picks and Odds for Week 5(2022)</h1>
+                            <p className='px-2 pb-2 text-sm'>There's nothing quite like the roller coaster of college football. you always think a game will go..</p>
+                        </div>
+                    </div>
+                    <div className='w-8/12 md:w-6/12 lg:w-4/12 xl:w-3/12  mx-auto p-4'>
+                        <div className='w-[250px] mx-auto rounded-lg border-[2px] border-[#1f2d37] hover:border-green-400'>
+                            <Image src='/image/Week-5-NCAAF-odds-1-250-140.jpg' width='400px' height='200px' alt=''/> 
+                            <h1 className='px-2 py-4'>College Footbvall Picks and Odds for Week 5(2022)</h1>
+                            <p className='px-2 pb-2 text-sm'>There's nothing quite like the roller coaster of college football. you always think a game will go..</p>
+                        </div>
+                    </div>
+                    <div className='w-8/12 md:w-6/12 lg:w-4/12 xl:w-3/12  mx-auto p-4'>
+                        <div className='w-[250px] mx-auto rounded-lg border-[2px] border-[#1f2d37] hover:border-green-400'>
+                           <Image src='/image/Week-5-NCAAF-odds-1-250-140.jpg' width='400px' height='200px' alt=''/>
+                            <h1 className='px-2 py-4'>College Footbvall Picks and Odds for Week 5(2022)</h1>
+                            <p className='px-2 pb-2 text-sm'>There's nothing quite like the roller coaster of college football. you always think a game will go..</p>
+                        </div>
+                    </div>
+                </div>
 
             </Container>
         </Section>
