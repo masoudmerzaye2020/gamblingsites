78dec5f5730e77087b2ba73524db2ae80976a8fd
Masoud Merzaye <60458825+masoudmerzaye2020@users.noreply.github.com>
2022-10-06 17:27:05 +0700

    horse racing

--- pages/horse-racing/index.js
+++ pages/horse-racing/index.js
@@ -1,8 +1,141 @@
 import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import Section from '../../components/section'
+import Container from '../../components/container'
+import Title from '../../components/title'
+import Header from '../../components/header'
+import Button from '../../components/button'
+import Wideimage from '../../components/wideimage'
+import Borderedbox from '../../components/borderedbox'
+import Mainbullet from '../../components/mainbullet'
+import Bluebutton from '../../components/bluebutton'
+import TextTitle from '../../components/TextTitle'
 
 function index() {
   return (
-    <div>horse racing</div>
+    <>
+      <Header>Guide to Horse Racing Betting</Header>
+      <Section>
+        <Container>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+
+          <Title title={''}/>
+          <p></p>
+          <p></p>
+          <p></p>
+          <Borderedbox>
+            <div>
+              <Mainbullet link={''} title={''}/>
+              <Mainbullet link={''} title={''}/>
+              <Mainbullet link={''} title={''}/>
+              <Mainbullet link={''} title={''}/>
+              <Mainbullet link={''} title={''}/>
+              <Mainbullet link={''} title={''}/>
+            </div>
+          </Borderedbox>
+
+          <div>
+            <Bluebutton link={''} title={''}/>
+          </div>
+          <p></p>
+          <div>
+            <Bluebutton link={''} title={''}/>
+          </div>
+
+          <Title title={''}/>
+          <p></p>
+          <p></p>
+
+          {/* leave recommened */}
+
+          <p></p>
+
+          {/* leave country cards */}
+
+          <Button link='' title="all recommended sites"/>
+          <p></p>
+          <div>
+            <Bluebutton link={''} title={''}/>
+            <Bluebutton link={''} title={''}/>
+            <Bluebutton link={''} title={''}/>
+          </div>
+
+          <p></p>
+
+          <Borderedbox>
+            <div>
+              <Mainbullet link={''} title={''}/>
+              <Mainbullet link={''} title={''}/>
+              <Mainbullet link={''} title={''}/>
+              <Mainbullet link={''} title={''}/>
+              <Mainbullet link={''} title={''}/>
+              <Mainbullet link={''} title={''}/>
+            </div>
+          </Borderedbox>
+
+          <div>
+            <Bluebutton link={''} title={''}/>
+          </div>
+
+          <Title title={''}/>
+          <p></p>
+          <p></p>
+
+          <div>
+           <Bluebutton link={''} title={''}/>
+          </div>
+
+          <TextTitle title={''}/>
+          <Borderedbox>
+            <div>
+              <Mainbullet link={''} title={''}/>
+              <Mainbullet link={''} title={''}/>
+              <Mainbullet link={''} title={''}/>
+              <Mainbullet link={''} title={''}/>          
+            </div>
+          </Borderedbox>
+          <Button link='' title=" "/>
+
+          <TextTitle title={''}/>
+          <Borderedbox>
+            <div>
+              <Mainbullet link={''} title={''}/>
+              <Mainbullet link={''} title={''}/>
+              <Mainbullet link={''} title={''}/>
+              <Mainbullet link={''} title={''}/>          
+            </div>
+          </Borderedbox>
+          <Button link='' title=" "/>
+
+          <TextTitle title={''}/>
+          <Borderedbox>
+            <div>
+              <Mainbullet link={''} title={''}/>
+              <Mainbullet link={''} title={''}/>
+              <Mainbullet link={''} title={''}/>
+              <Mainbullet link={''} title={''}/>          
+            </div>
+          </Borderedbox>
+          <Button link='' title=" "/>
+
+
+          <Title title={''}/>
+          <p></p>
+          <p></p>
+
+          <Borderedbox> </Borderedbox>
+
+          <p></p>
+
+
+        </Container>
+      </Section>
+    </>
   )
 }
 
