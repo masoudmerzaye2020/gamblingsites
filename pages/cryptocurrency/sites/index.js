21008d55402f3cd10880d942f573bd24240602a8
Masoud Merzaye <60458825+masoudmerzaye2020@users.noreply.github.com>
2022-10-06 19:59:08 +0700

    cryptocurrency sites

--- pages/cryptocurrency/sites/index.js
+++ pages/cryptocurrency/sites/index.js
@@ -1,9 +1,189 @@
 import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import Section from '../../../components/section'
+import Container from '../../../components/container'
+import Title from '../../../components/title'
+import TextTitle from '../../../components/TextTitle'
+import Header from '../../../components/header'
+import Button from '../../../components/button'
+import Bluebutton from '../../../components/bluebutton'
+import Mainbullet from '../../../components/mainbullet'
+import Borderedbox from '../../../components/borderedbox'
+import Wideimage from '../../../components/wideimage'
 
-function index() {
+function Index() {
   return (
-    <div>best</div>
+    <>
+      <Header>Best Cryptocurrency Gambling Sites in 2022</Header>
+      <Section>
+        <Container>
+          <p></p>
+          <p></p>
+          {/* LEAVE THE ADD */}
+          <p></p>
+          <p></p>
+          <div>
+            <Mainbullet link={''} linkdesc={''}/>
+            <Mainbullet link={''} linkdesc={''}/>
+            <Mainbullet link={''} linkdesc={''}/>
+            <Mainbullet link={''} linkdesc={''}/>
+            <Mainbullet link={''} linkdesc={''}/>
+            <Mainbullet link={''} linkdesc={''}/>
+          </div>
+
+          <Title title={''}/>
+          <p></p>
+
+          {/*  leave the table */}
+          <p></p>
+          <div>
+            <Mainbullet link={''} desc={''}/>
+            <Mainbullet link={''} desc={''}/>
+            <Mainbullet link={''} desc={''}/>
+            <Mainbullet link={''} desc={''}/>
+            <Mainbullet link={''} desc={''}/>
+          </div>
+          <p></p>
+          <Title title={''}/>
+          <p></p>
+
+          {/* leave the table */}
+
+          <p></p>
+          <p></p>
+
+          <Borderedbox> </Borderedbox>
+
+          <Title title={''}/>
+          <p></p>
+
+          <TextTitle title={''}/>
+          <p></p>
+          {/* leave the table */}
+
+          <p></p>
+
+          <div className='bg-[#e9ffee]'>
+            <Mainbullet link={''} linkdesc={''} desc={''}/>
+            <Mainbullet link={''} linkdesc={''} desc={''}/>
+            <Mainbullet link={''} linkdesc={''} desc={''}/>
+            <Mainbullet link={''} linkdesc={''} desc={''}/>
+          </div>
+
+          <TextTitle title={''}/>
+          <p></p>
+
+          {/* leave the bellow style */}
+
+          <p></p>
+
+          <Title title={''}/>
+          <p></p>
+          <p></p>
+          <p></p>
+
+          {/* leave the box style */}
+
+          <p></p>
+
+          {/* Image place */}
+
+          <p></p>
+
+          {/* leave the red box */}
+
+          <p></p>
+          <p></p>
+
+          <Title title={''}/>
+          <p></p>
+          <div>
+            <Mainbullet link={''} desc={''}/>
+            <Mainbullet link={''} desc={''}/>
+          </div>
+          <p></p>
+
+          <TextTitle title={''}/>
+
+          <p></p>
+          <p></p>
+
+          {/* image place */}
+
+          <p></p>
+          <p></p>
+
+          <TextTitle title={''}/>
+
+          <p></p>
+          <p></p>
+          {/* leave the table */}
+
+          <p></p>
+
+          <Title title={''}/>
+          <p></p>
+
+          <TextTitle title={''}/>
+
+          <div className='bg-[#e9ffee]'>
+            <Mainbullet link={''} title={''} desc={''}/>
+            <Mainbullet link={''} title={''} desc={''}/>
+            <Mainbullet link={''} title={''} desc={''}/>
+            <Mainbullet link={''} title={''} desc={''}/>
+            <Mainbullet link={''} title={''} desc={''}/>
+          </div>
+
+          <p></p>
+
+          <TextTitle title={''}/>
+          <p></p>
+          <p></p>
+
+          <TextTitle title={''}/>
+          <p></p>
+
+          <div>
+            <Mainbullet link={''} desc={''}/>
+            <Mainbullet link={''} desc={''}/>
+            <Mainbullet link={''} desc={''}/>
+            <Mainbullet link={''} desc={''}/>
+            <Mainbullet link={''} desc={''}/>
+            <Mainbullet link={''} desc={''}/>
+          </div>
+
+          <p></p>
+          {/* image here */}
+
+          <TextTitle title={''}/>
+          <p></p>
+          <p></p>
+
+          <Title title={''}/>
+
+          {/* leave the FAQ */}
+
+          <Title title={''}/>
+          <p></p>
+          <p></p>
+
+          <div className='bg-[#e4f8ff]'>
+            <Mainbullet link={''} linkdesc={''}/>
+            <Mainbullet link={''} linkdesc={''}/>
+            <Mainbullet link={''} linkdesc={''}/>
+            <Mainbullet link={''} linkdesc={''}/>
+          </div>
+
+          
+
+          
+
+
+        </Container>
+      </Section>
+    </>
   )
 }
 
-export default index
+export default Index
