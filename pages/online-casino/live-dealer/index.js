951970ce0f9a3c5515e210c33e8939951a350403
Masoud Merzaye <60458825+masoudmerzaye2020@users.noreply.github.com>
2022-10-06 18:08:43 +0700

    live dealer

--- pages/online-casino/live-dealer/index.js
+++ pages/online-casino/live-dealer/index.js
@@ -1,9 +1,217 @@
 import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import Section from '../../../components/section'
+import Container from '../../../components/container'
+import Title from '../../../components/title'
+import TextTitle from '../../../components/TextTitle'
+import Header from '../../../components/header'
+import Button from '../../../components/button'
+import Bluebutton from '../../../components/bluebutton'
+import Mainbullet from '../../../components/mainbullet'
+import Borderedbox from '../../../components/borderedbox'
+import Wideimage from '../../../components/wideimage'
 
-function index() {
+function Index() {
   return (
-    <div>live dealer</div>
+    <>
+      <Header>Best Live Online Casinos with Human Dealers</Header>
+      <Section>
+        <Container>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+
+          <Title title={''}/>
+          <div>
+            <Mainbullet link={''} linkdesc={''}/>
+            <Mainbullet link={''} linkdesc={''}/>
+            <Mainbullet link={''} linkdesc={''}/>
+            <Mainbullet link={''} linkdesc={''}/>
+          </div>
+          <Wideimage src={'/image/'}/>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <Title title={''}/>
+          <p></p>
+          <p></p>
+          <p></p>
+
+          <div>
+            <Mainbullet link={''} linkdesc={''} desc={''} />
+            <Mainbullet link={''} linkdesc={''} desc={''} />
+            <Mainbullet link={''} linkdesc={''} desc={''} />
+          </div>
+          <p></p>
+          <p></p>
+          {/*  leave the small card */}
+
+          <TextTitle title={''}/>
+          <p></p>
+          <div>
+            <Mainbullet link={''} linkdesc={''} title={''}/>
+            <Mainbullet link={''} linkdesc={''} title={''}/>
+            <Mainbullet link={''} linkdesc={''} title={''}/>
+            <Mainbullet link={''} linkdesc={''} title={''}/>
+            <Mainbullet link={''} linkdesc={''} title={''}/>
+            <Mainbullet link={''} linkdesc={''} title={''}/>
+            <Mainbullet link={''} linkdesc={''} title={''}/>
+            <Mainbullet link={''} linkdesc={''} title={''}/>
+            <Mainbullet link={''} linkdesc={''} title={''}/>
+            <Mainbullet link={''} linkdesc={''} title={''}/>
+          </div>
+          <p></p>
+
+          <TextTitle title={''}/>
+          <p></p>
+
+          <div>
+            <Mainbullet link={''} title={''} desc={''}/>
+            <Mainbullet link={''} title={''} desc={''}/>
+            <Mainbullet link={''} title={''} desc={''}/>
+            <Mainbullet link={''} title={''} desc={''}/>
+            <Mainbullet link={''} title={''} desc={''}/>
+          </div>
+
+          <p></p>
+
+          <Title title={''}/>
+          <Wideimage src={'/image/'}/>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+
+          <Title title={''}/>
+          <p></p>
+          <div>
+            <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+            <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+            <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+            <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+            <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          </div>
+          <p></p>
+
+          <TextTitle title={''}/>
+          <p></p>
+          <div>
+            <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+            <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+            <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+            <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+            <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+            <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+            <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+            <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+            <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+            <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+
+          </div>
+          <p></p>
+          <p></p>
+
+          <Title title={''}/>
+          <Wideimage src={'/image/'}/>
+          <p></p>
+          <p></p>
+          <div>
+            <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+            <p></p>
+            <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+            <p></p>
+            <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+            <p></p>
+            <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+            <p></p>
+            <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+            <p></p>
+            <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+            <p></p>
+          </div>
+          <p></p>
+          <p></p>
+
+          <Title title={''}/>
+          <Wideimage src={'/image/'}/>
+          <p></p>
+          <div>
+            <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+            <p></p>
+            <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+            <p></p>
+            <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+            <p></p>
+            <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+            <p></p>
+            <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+            <p></p>
+            <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+            <p></p>
+          </div>
+
+          <p></p>
+          <p></p>
+          <p></p>
+
+          <Title title={''}/>
+          <Wideimage src={'/image/'}/>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+
+          {/* leave provile card */}
+          <Title title={''}/>
+          <Wideimage src={'/image/'}/>
+          <p></p>
+          <div>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          </div>
+          <p></p>
+          <p></p>
+          <p></p>
+
+          <Title title={''}/>
+          <Wideimage src={'/image/'}/>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+
+          <TextTitle title={''}/>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+
+          <Title title={''}/>
+
+          {/* leave FAQ */}
+
+          <Title title={''}/>
+          <p></p>
+          <p></p>
+
+          {/* leave youtube video */}
+
+
+          
+        </Container>
+      </Section>
+    </>
   )
 }
 
-export default index
+export default Index
