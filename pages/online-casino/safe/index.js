5f1d1201aadf9fd40e90c9c98f78bc5499e40e44
Masoud Merzaye <60458825+masoudmerzaye2020@users.noreply.github.com>
2022-10-06 18:55:25 +0700

    online casino safe

--- pages/online-casino/safe/index.js
+++ pages/online-casino/safe/index.js
@@ -1,9 +1,244 @@
 import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import Section from '../../../components/section'
+import Container from '../../../components/container'
+import Title from '../../../components/title'
+import TextTitle from '../../../components/TextTitle'
+import Header from '../../../components/header'
+import Button from '../../../components/button'
+import Bluebutton from '../../../components/bluebutton'
+import Mainbullet from '../../../components/mainbullet'
+import Borderedbox from '../../../components/borderedbox'
+import Wideimage from '../../../components/wideimage'
 
-function index() {
+function Index() {
   return (
-    <div>online casino safe</div>
+    <>
+    <Header></Header>
+    <Section>
+      <Container>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <Title title={''}/>
+        <p></p>
+        <Borderedbox> </Borderedbox>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+
+        <TextTitle title={''}/>
+        <p></p>
+        {/* leave the table */}
+
+        <p></p>
+
+        <TextTitle title={''}/>
+        <p></p>
+
+        <div>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+        </div>
+        <p></p>
+        <p></p>
+        <p></p>
+
+        <div>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+        
+        </div>
+
+        <p></p>
+        <p></p>
+
+        <TextTitle title={''}/>
+
+        <p></p>
+        <p></p>
+        <div>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+        
+        </div>
+        <p></p>
+
+        <TextTitle title={''}/>
+        <div>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+        </div>
+
+        <TextTitle title={''}/>
+        <p></p>
+        <p></p>
+        <div>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+        
+        </div>
+
+        <p></p>
+        <p></p>
+        <p></p>
+        <Borderedbox></Borderedbox>
+
+        <p></p>
+        <p></p>
+
+        <Title title={''}/>
+        <p></p>
+        <div>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+        </div>
+
+        <Title title={''}/>
+        <div>
+          <p></p>
+          <p></p>
+          <p></p>
+        </div>
+
+        <div className='bg-[#e9ffee]'>
+          <h2>Benefits of Online Casinos with Reputable Software</h2>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+        </div>
+
+        <Title title={''}/>
+        <div>
+          <p></p>
+          <p></p>
+        </div>
+
+        <div>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+        </div>
+
+        <Title title={''}/>
+
+        <p></p>
+        <p></p>
+
+        {/* leave the table */}
+
+        <TextTitle title={''}/>
+        <div>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+        </div>
+
+        <TextTitle title={''}/>
+        <div>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+        </div>
+
+        <div className='bg-[#e9ffee]'>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+
+        </div>
+
+        <p></p>
+        <p></p>
+        {/* leave the icon */}
+
+        <p></p>
+        <p></p>
+
+        <Title title={''}/>
+        <div>
+          <p></p>
+          {/* leave image */}
+        </div>
+
+        <div className='bg-[#ffefef]'>
+          <p></p>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+          <Mainbullet link={''} linkdesc={''} title={''} desc={''}/>
+
+        </div>
+
+        <p></p>
+        <p></p>
+
+        <Title title={''}/>
+
+        {/* leave the FAQ */}
+
+
+
+
+
+
+
+      </Container>
+    </Section>
+    </>
   )
 }
 
-export default index
+export default Index
