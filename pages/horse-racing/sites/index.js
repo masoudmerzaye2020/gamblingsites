6b33fd893edba5faf4bb06c1d33b223696e25f57
Masoud Merzaye <60458825+masoudmerzaye2020@users.noreply.github.com>
2022-10-06 16:56:43 +0700

    horse beting

--- pages/horse-racing/sites/index.js
+++ pages/horse-racing/sites/index.js
@@ -1,8 +1,232 @@
 import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import Section from '../../../components/section'
+import Container from '../../../components/container'
+import Title from '../../../components/title'
+import TextTitle from '../../../components/TextTitle'
+import Header from '../../../components/header'
+import Button from '../../../components/button'
+import Bluebutton from '../../../components/bluebutton'
+import Mainbullet from '../../../components/mainbullet'
+import Borderedbox from '../../../components/borderedbox'
+import Wideimage from '../../../components/wideimage'
 
 function index() {
   return (
-    <div>horse racing sites</div>
+    <>
+      <Header>Top Horse Racing Betting Sites in 2022</Header>
+      <Section>
+        <Container>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+
+          <Title title={''}/>
+
+          <div>
+            <Mainbullet link={''} linkdesc={''}/>
+            <Mainbullet link={''} linkdesc={''}/>
+            <Mainbullet link={''} linkdesc={''}/>
+            <Mainbullet link={''} linkdesc={''}/>
+            <Mainbullet link={''} linkdesc={''}/>
+          </div>
+          <Wideimage src={'/image/'}/>
+
+          <p></p>
+          <p></p>
+          <p></p>
+
+          <Title title={''}/>
+          <p></p>
+          <p></p>
+          <p></p>
+
+
+          <TextTitle title={''}/>
+          <p></p>
+          <p></p>
+
+          <TextTitle title={''}/>
+          <p></p>
+          <p></p>
+          <p></p>
+
+          <TextTitle title={''}/>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+
+          {/* leave the list */}
+
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+
+          <Title title={''}/>
+          <p></p>
+          <p></p>
+          <p></p>
+
+          <div>
+            <Bluebutton link={''} title={''}/>
+            <Bluebutton link={''} title={''}/>
+            <Bluebutton link={''} title={''}/>
+          </div>
+
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+
+          <div>
+            <Bluebutton link={''} title={''}/>
+            <Bluebutton link={''} title={''}/>
+            <Bluebutton link={''} title={''}/>
+            <Bluebutton link={''} title={''}/>
+          </div>
+
+          <p></p>
+          <p></p>
+
+          <div>
+            <Bluebutton link={''} title={''}/>
+            <Bluebutton link={''} title={''}/>
+            <Bluebutton link={''} title={''}/>
+            <Bluebutton link={''} title={''}/>
+          </div>
+
+          <p></p>
+
+          <Title title={''}/>
+          <p></p>
+          <p></p>
+          <p></p>
+
+          <TextTitle title={''}/>
+          <p></p>
+          <p></p>
+
+          <div>
+            <Mainbullet link={''} title={''}/>
+            <Mainbullet link={''} title={''}/>
+            <Mainbullet link={''} title={''}/>
+            <Mainbullet link={''} title={''}/>
+            <Mainbullet link={''} title={''}/>
+            <Mainbullet link={''} title={''}/>
+            <Mainbullet link={''} title={''}/>
+            <Mainbullet link={''} title={''}/>
+            <Mainbullet link={''} title={''}/>
+          </div>
+
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+
+          <TextTitle title={''}/>
+          <p></p>
+          <p></p>
+          <p></p>
+
+          <div>
+            <Bluebutton link={''} title={''}/>
+          </div>
+
+          <p></p>
+          <p></p>
+          <p></p>
+
+          <TextTitle title={''}/>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+
+          <TextTitle title={''}/>
+
+          <p></p>
+          <p></p>
+          <Mainbullet link={''} title={''}/>
+          <p></p>
+          <Mainbullet link={''} title={''}/>
+          <p></p>
+          <Mainbullet link={''} title={''}/>
+          <p></p>
+          <Mainbullet link={''} title={''}/>
+          <p></p>
+          <Mainbullet link={''} title={''}/>
+          <p></p>
+          <Mainbullet link={''} title={''}/>
+          <p></p>
+          <Mainbullet link={''} title={''}/>
+          <p></p>
+          <Mainbullet link={''} title={''}/>
+          <p></p>
+          <Mainbullet link={''} title={''}/>
+          <p></p>
+          <Mainbullet link={''} title={''}/>
+          <p></p>
+          <p></p>
+
+
+          <TextTitle title={''}/>
+          <p></p>
+          <p></p>
+          <p></p>
+
+          <Mainbullet link={''} title={''}/>
+          <p></p>
+          <Mainbullet link={''} title={''}/>
+          <p></p>
+          <Mainbullet link={''} title={''}/>
+          <p></p>
+          <Mainbullet link={''} title={''}/>
+          <p></p>
+          <Mainbullet link={''} title={''}/>
+          <p></p>
+          <Mainbullet link={''} title={''}/>
+          <p></p>
+          <Mainbullet link={''} title={''}/>
+          <p></p>
+          <Mainbullet link={''} title={''}/>
+          <p></p>
+          <Mainbullet link={''} title={''}/>
+          <p></p>
+          <Mainbullet link={''} title={''}/>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+
+          <Title title={''}/>
+          {/* leave the FQA */}
+
+          <Title title={''}/>
+
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+
+
+
+        </Container>
+      </Section>
+    </>
   )
 }
 
