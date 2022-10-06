fd5784913f8c3d0d885c8257baaa0682823adbd3
Masoud Merzaye <60458825+masoudmerzaye2020@users.noreply.github.com>
2022-10-06 16:07:01 +0700

    bonuses

--- pages/sports-betting/sites/bonuses-rewards/index.js
+++ pages/sports-betting/sites/bonuses-rewards/index.js
@@ -1,8 +1,246 @@
 import React from 'react'
+import Header from '../../../../components/header'
+import Section from '../../../../components/section'
+import Container from '../../../../components/container'
+import Title from '../../../../components/title'
+import TextTitle from '../../../../components/TextTitle'
+import Bluebutton from '../../../../components/bluebutton'
+import Mainbullet from '../../../../components/mainbullet'
+import Wideimage from '../../../../components/wideimage'
+import Borderedbox from '../../../../components/borderedbox'
 
 function index() {
   return (
-    <div>bonuses rewardx</div>
+    <>
+      <Header>Best Sports Betting Sites With Bonuses</Header>
+      <Section>
+        <Container>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+
+          {/* leave the overview */}
+
+          <Title title={''}/>
+          <p></p>
+          <p></p>
+          <p></p>
+
+          <TextTitle title={''}/>
+          <p></p>
+          <p></p>
+          {/* leave the table */}
+          <p></p>
+          {/* leave the list */}
+          <p></p>
+          <p></p>
+
+          {/* leave the boxes */}
+          <p></p>
+
+          <TextTitle title={''}/>
+          <p></p>
+
+          <Bluebutton link={''} title={''}/>
+          <p></p>
+          <p></p>
+          <p></p>
+
+          {/* leave the image box */}
+
+          <Title title={''}/>
+          <Wideimage src={'/image/'}/>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+
+          {/* leave the list */}
+
+          <TextTitle title={''}/>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+
+          {/* leave the top tip */}
+
+          <TextTitle title={''}/>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+
+          <TextTitle title={''}/>
+          <p></p>
+          <p></p>
+
+          <Borderedbox> </Borderedbox>
+
+          <p></p>
+          <p></p>
+          <p></p>
+
+          {/* leave the list */}
+
+          <TextTitle title={''}/>
+          <p></p>
+          <p></p>
+          <p></p>
+
+          {/* leave the table */}
+
+          <TextTitle title={''}/>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+
+          <TextTitle title={''}/>
+          <p></p>
+          <p></p>
+          <p></p>
+
+          <Borderedbox> </Borderedbox>
+
+          <TextTitle title={''}/>
+          <p></p>
+          <p></p>
+          <p></p>
+
+          <TextTitle title={''}/>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+
+          {/* leave the blue list */}
+
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+
+          {/* leave the table */}
+          <p></p>
+
+          <Title title={''}/>
+          <Wideimage src={'/image'}/>
+          <p></p>
+          <p></p>
+          <p></p>
+
+          <TextTitle title={''}/>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+
+          {/* leave the image box */}
+
+          <p></p>
+          <p></p>
+
+          <TextTitle title={''}/>
+          <p></p>
+          <p></p>
+
+          {/* leave the list */}
+
+          <p></p>
+          <p></p>
+          <p></p>
+
+          <TextTitle title={''}/>
+          <p></p>
+          <p></p>
+          <p></p>
+
+          {/* leave the list */}
+
+          <p></p>
+          <p></p>
+
+          {/* leave top tip */}
+
+          <TextTitle title={''}/>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          {/* leave the list */}
+          <p></p>
+          <p></p>
+
+          <Title title={''}/>
+          <Wideimage src={'/image'}/>
+          <p></p>
+          <p></p>
+          <p></p>
+
+          <Mainbullet link={''} title={''}/>
+          <Mainbullet link={''} title={''}/>
+
+          <p></p>
+          <p></p>
+          <p></p>
+
+          <TextTitle title={''}/>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+
+          <Borderedbox> </Borderedbox>
+
+          <p></p>
+          <p></p>
+
+          <TextTitle title={''}/>
+          <p></p>
+          <p></p>
+
+          <TextTitle title={''}/>
+          <p></p>
+          <p></p>
+
+          <TextTitle title={''}/>
+          <p></p>
+          <p></p>
+
+          <TextTitle title={''}/>
+          <p></p>
+          <p></p>
+
+          <TextTitle title={''}/>
+          <p></p>
+          <p></p>
+
+          <TextTitle title={''}/>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+
+          <Title title={''}/>
+
+          {/* leave the table */}
+
+
+
+
+
+        </Container>
+      </Section>
+    </>
   )
 }
 
