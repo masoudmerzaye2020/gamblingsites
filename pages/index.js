b2d29f6615caf0ff0637e35f806b2b9a9ca9d761
Masoud Merzaye <dreamteam.freelancer@gmail.com>
2022-10-24 11:28:33 +0700

    commit

--- pages/index.js
+++ pages/index.js
@@ -125,7 +125,7 @@ export default function Home() {
               <RegionalCard title={'sports betting sites with bonuses'} url={''} src={'/image/bonuses-icon-multiple-buttons-1.png'} altText={''}/>
               <RegionalCard title={'casino bonuses and bonus codes'} url={''} src={'/image/bonus-codes-icon-multiple-buttons-1.png'} altText={''}/>
             </div>
-            <Title title={'Is Online Gambling Safe?'}/>'
+            <Title title={'Is Online Gambling Safe?'}/>
             <p>The most important question we get asked is this one. Everyone wants to know, “is online gambling safe,” and we understand why so many people are concerned about it. So, we want to answer this question straight away.</p>
                 <p>Online gambling is safe as long as you use safe gambling sites. But it can be difficult to know which sites are safe and which ones shouldn’t be trusted. If you want to take the easy route, you can simply trust our recommendations for the safest gambling sites. Or you can keep reading so we can explain how to find the safest sites for yourself.</p>
                 <p>There are several factors we look for when finding the safest gambling websites. First of all, the site has to be licensed. Gambling authorities like the UK Gambling Commission or the Curacao e-Gaming Authority hold sites accountable to their standards. They have authority over sites that individual players lack, so they can force sites to protect players in various ways.</p>
@@ -139,7 +139,7 @@ export default function Home() {
                 <p>We also look for sites that take security seriously. SSL encryption techniques are the industry standard because they protect your financial and personal information. The site should also have a solid privacy policy.</p>
                 <p>Some sites go above and beyond by offering information about dispute systems or game auditing. Dispute systems can act as a mediator if you have an issue with a site. Game auditing certificates show you that the site’s random number generators and game software have been tested regularly to make sure they are fair.</p>
                 <p>The safest gambling sites also give you options. For example, you should be able to opt-out of emails from the site and choose from several options for verification documents. Sites that restrict those options are probably up to something. And avoiding the wrong sites is just as important as finding the safest gambling sites.</p>
-              '
+              
           
           
           <Title title={"Detailed Gambling Guides from Expert Gamblers"} />
